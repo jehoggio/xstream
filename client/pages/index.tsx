861f81f293fa2c865d94bd70c068bbd7fa2df0ee
jeff <jeffhogg86@gmail.com>
2021-10-30 16:28:58 -0700

    adds layout component

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,8 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Layout from '../components/Layout'
 
 const Home: NextPage = () => {
-  return <div className="bg-yellow-200">Home</div>
+  return <Layout>Home</Layout>
 }
 
 export default Home
